a292b0e8336586b4fff89677e520dde7c2de6f61
Yaroslav Konoplov <yaroslav@inbox.com>
2016-05-29 12:53:45 +0300

    Add fix which allow create inheritance classes with 'new'

--- lib/integer-inheritance/extension.rb
+++ lib/integer-inheritance/extension.rb
@@ -15,10 +15,27 @@ module IntegerInheritance
         inheritance_mapping.key(v) || v
       end
 
+      alias inheritance_type sti_name
+
       # http://apidock.com/rails/ActiveRecord/Inheritance/ClassMethods/find_sti_class
-      def find_sti_class(type_name)
-        lookup = inheritance_mapping[type_name.to_i]
+      def find_sti_class(type)
+        lookup = inheritance_mapping[type.to_i]
         lookup ? super(lookup) : super
+      end
+
+    private
+      def subclass_from_attributes(attrs)
+        type = attrs.with_indifferent_access[inheritance_column].to_i
+
+        if type && type != inheritance_type
+          subclass = inheritance_mapping[type].safe_constantize
+          unless descendants.include?(subclass)
+            raise ActiveRecord::SubclassNotFound, %{ Invalid single-table inheritance type:
+                                                     either subclass can't be mapped to #{type}
+                                                     either #{subclass} is not a subclass of #{name}}.trim.squish
+          end
+          subclass
+        end
       end
     end
   end
